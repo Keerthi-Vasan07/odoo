7fb62cc105c3d1862121f1b46595e93e2073e247
Keerthi Vasan <keerthivasan2197@gmail.com>
2026-05-10 13:14:49 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,39 +1,7 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { useAuthStore } from './store/authStore';
-import Navbar from './components/Navbar';
-import Login from './pages/Login_screen_1';
-import Register from './pages/Registration_screen_2';
-import Dashboard from './pages/Main_Landing_page_screen_3';
-import CreateTrip from './pages/Create_a_new_trip_screen_4';
-import TripList from './pages/User_Trip_Listing_screen_6';
-import ItineraryBuilder from './pages/Build_Itenary_screen_5';
-
-const ProtectedRoute = ({ children }) => {
-  const token = useAuthStore(state => state.token);
-  return token ? children : <Navigate to="/login" />;
-};
+import TestPage from "./Testpage";
 
 function App() {
-  const token = useAuthStore(state => state.token);
-
-  return (
-    <Router>
-      <div className="min-h-screen bg-gray-50 flex flex-col">
-        {token && <Navbar />}
-        <main className="flex-1 w-full max-w-7xl mx-auto p-4 md:p-8">
-          <Routes>
-            <Route path="/login" element={!token ? <Login /> : <Navigate to="/" />} />
-            <Route path="/register" element={!token ? <Register /> : <Navigate to="/" />} />
-            
-            <Route path="/" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
-            <Route path="/trips" element={<ProtectedRoute><TripList /></ProtectedRoute>} />
-            <Route path="/trips/new" element={<ProtectedRoute><CreateTrip /></ProtectedRoute>} />
-            <Route path="/trips/:id/build" element={<ProtectedRoute><ItineraryBuilder /></ProtectedRoute>} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
-  );
+  return <TestPage />;
 }
 
 export default App;
---
+++ frontend/src/TestPage.jsx
@@ -0,0 +1,5 @@
+import ExpenseInvoice from "./pages/expense_invoice_screen_14";
+
+export default function TestPage() {
+  return <ExpenseInvoice />;
+}
